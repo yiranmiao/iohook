b6201033a3edbfb74539421a790e2569b7bd077c
shirker2006@gmail.com <shirker2006@gmail.com>
2023-08-18 18:06:01 +0800

    up

--- abi.js
+++ abi.js
@@ -1,2 +1,7 @@
 const nodeAbi = require('node-abi')
-console.log(nodeAbi.getAbi('22.0.0', 'electron'))
+console.log('ELECTRON 22.0.0: ', nodeAbi.getAbi('22.0.0', 'electron'))
+console.log('ELECTRON 23.0.0: ', nodeAbi.getAbi('23.0.0', 'electron'))
+console.log('NODE 18.0.0: ', nodeAbi.getAbi('18.0.0', 'node')) //108
+console.log('NODE 16.0.0: ', nodeAbi.getAbi('16.0.0', 'node')) //93
+console.log('NODE 19.0.0: ', nodeAbi.getAbi('19.0.0', 'node'))
+console.log('NODE 20.0.0: ', nodeAbi.getAbi('20.0.0', 'node'))
